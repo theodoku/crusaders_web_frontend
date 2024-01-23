72d058cb1f992ad08fe70ef2098d266d9ab2c823
theophilus doku <theodoku89@gmail.com>
2024-01-23 17:16:34 +0000

    Modified app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import LoginPage from './pages/LoginPage';
+import ProtectedRoute from './pages/ProtectedRoute';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import Persons from './pages/Persons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <ToastContainer />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            (
+              <ProtectedRoute>
+                <Persons />
+              </ProtectedRoute>
+            )
+          }
+        />
+        <Route path="/Persons" element={<Persons />} />
+        <Route path="/register" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
