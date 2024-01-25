fccb7aa375f441e7e0ae0269dab5646b40e8c329
theophilus doku <theodoku89@gmail.com>
2024-01-25 10:57:18 +0000

    Modify NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,14 +18,14 @@ const NavBar = () => {
       });
   };
   return (
-    <div className="w-full flex flex-col items-center">
-      <div className="p-4 w-full bg-base-200 shadow">
+    <div className="flex flex-col nav">
+      <div className="p-4 bg-base-100 shadow">
         <Link to="/">
-          <img src={Logo} alt="Logo" />
+          <img src={Logo} alt="Logo" className="logo-small" />
         </Link>
       </div>
 
-      <div className="p-4 w-full flex flex-col items-center">
+      <div className="p-5 flex flex-row items-center justify-center nav">
         <NavLink to="/" className="p-3 text-xl rounded-none shadow-sm hover:bg-[#97bf0f]">
           HOME
         </NavLink>
@@ -41,32 +41,25 @@ const NavBar = () => {
         <NavLink to="/contact-us" className="p-3 text-xl rounded-none hover:bg-[#97bf0f]">
           CONTACT US
         </NavLink>
-      </div>
-
-      <div className="p-4">
-        <Link to="/register">
-          <button
-            type="button"
-            onClick={handleLogout}
-            className="btn flex items-center p-1 gap-2 hover:border-xl hover:text-white hover:bg-red-500 shadow bg-red-100 text-red-500"
-          >
-            Logout
-            <BiLogOutCircle className="text-3xl" />
-          </button>
-        </Link>
-      </div>
-
-      <div className="grid gap-4">
-        <span className="flex gap-4">
-          <BsTwitter className="text-x1" />
-          <FaFacebookF className="text-x1" />
-          <BsWhatsapp className="text-x1" />
-        </span>
-        <span className="text-sm font-bold">
-          &copy;
-          {' '}
-          <small>Team Crusaders</small>
-        </span>
+        <div className="p-1 ml-10">
+          <Link to="/register">
+            <button
+              type="button"
+              onClick={handleLogout}
+              className="btn flex items-center p-1 gap-2 hover:border-xl hover:text-white hover:bg-red-500 shadow bg-red-100 text-red-500"
+            >
+              Logout
+              <BiLogOutCircle className="text-3xl" />
+            </button>
+          </Link>
+        </div>
+        <div className="flex items-center ml-12">
+          <div className="flex gap-4">
+            <BsTwitter className="text-x1" />
+            <FaFacebookF className="text-x1" />
+            <BsWhatsapp className="text-x1" />
+          </div>
+        </div>
       </div>
     </div>
   );
