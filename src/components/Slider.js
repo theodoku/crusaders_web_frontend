1173f39d474fd966772da34ad15302d11e1bf3be
theophilus doku <theodoku89@gmail.com>
2024-02-11 18:31:36 +0000

    Modify Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import 'swiper/css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import 'swiper/css/navigation';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import "swiper/css/navigation";
 import {
   TiSocialTwitterCircular,
   TiSocialPinterestCircular,
   TiSocialFacebookCircular,
-} from 'react-icons/ti';
-import axios from 'axios';
-import { selectUserId } from '../redux/userReducer';
+} from "react-icons/ti";
+import axios from "axios";
+import { selectUserId } from "../redux/userReducer";
 
 const Slider = () => {
   const [persons, setPersons] = useState([]);
@@ -23,23 +23,23 @@ const Slider = () => {
     const fetchPersons = async () => {
       try {
         if (userId !== null) {
-          const response = await axios.get(`http://127.0.0.1:3000/api/v1/users/${userId}/people`);
+          const response = await axios.get(
+            `http://127.0.0.1:3000/api/v1/users/${userId}/people`
+          );
           setPersons(response.data);
           setIsLoading(false);
-          if (response.data.length > 0) {
-            console.log('Person Photos:', response.data.map((person) => person.photo));
-          }
         } else {
           setIsLoading(false);
-          setError('User ID is null');
+          setError("User ID is null");
         }
       } catch (error) {
-        setError('Error fetching data');
+        setError("Error fetching data");
         setIsLoading(false);
       }
     };
     fetchPersons();
   }, [userId]);
+
   if (isLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
@@ -59,10 +59,10 @@ const Slider = () => {
   return (
     <div
       id="slider"
-      style={{ position: 'relative' }}
+      style={{ position: "relative" }}
       className="w-[85%] md:w-[95%]"
     >
-      <div style={{ width: '100%', position: 'relative' }}>
+      <div style={{ width: "100%", position: "relative" }}>
         <div className="slider-btns">
           <div className="swiper-prev">
             <IoIosArrowBack />
@@ -78,8 +78,8 @@ const Slider = () => {
           slidesPerGroup={1}
           modules={[Navigation]}
           navigation={{
-            nextEl: '.swiper-next',
-            prevEl: '.swiper-prev',
+            nextEl: ".swiper-next",
+            prevEl: ".swiper-prev",
           }}
           breakpoints={{
             768: {
@@ -94,6 +94,7 @@ const Slider = () => {
               key={`person-${index}`}
               className="pl-10 flex flex-col"
             >
+              {console.log("Person Data:", person)}
               <div className="wrapper">
                 <div className="slide-container">
                   <img
@@ -101,15 +102,16 @@ const Slider = () => {
                     src={person.photo}
                     alt={person.name}
                     style={{
-                      width: '100%',
-                      height: '550px',
-                      objectFit: 'cover',
+                      width: "100%",
+                      height: "550px",
+                      objectFit: "cover",
                     }}
                   />
                 </div>
                 <div className="person-info">
                   <h3 className="person-name">{person.name}</h3>
                   <p className="person-title">{person.title}</p>
+                  <p className="person-biography">{person.biography}</p>
                   <div className="slide-socials">
                     <TiSocialFacebookCircular fill="thistle" className="icon" />
                     <TiSocialTwitterCircular fill="thistle" className="icon" />
